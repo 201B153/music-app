c612368ffc2b4693a765049c8d26df7968169627
Mukul Kumar <201b162@juetguna.in>
2022-06-01 22:01:59 +0530

    Add files

---
+++ app/src/main/java/com/example/musicplayerlite/helper/VersioningHelper.java
@@ -0,0 +1,9 @@
+package com.example.musicplayerlite.helper;
+
+import android.os.Build;
+
+public class VersioningHelper {
+    public static boolean isVersionQ() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q;
+    }
+}
---
+++ app/src/main/java/com/example/musicplayerlite/listener/PlayListListener.java
@@ -0,0 +1,9 @@
+package com.example.musicplayerlite.listener;
+
+import android.content.Context;
+
+import com.example.musicplayerlite.model.Music;
+
+public interface PlayListListener {
+    void option(Context context, Music music);
+}
