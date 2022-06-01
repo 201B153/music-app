a41e607608a1cf410fe72c6ebfc3331adc068b0c
Mukul Kumar <201b162@juetguna.in>
2022-06-01 22:05:25 +0530

    final working build

---
+++ app/src/main/java/com/example/musicplayerlite/player/PlayerProgressEvent.java
@@ -0,0 +1,9 @@
+package com.example.musicplayerlite.player;
+
+public class PlayerProgressEvent {
+    public final int percent;
+
+    public PlayerProgressEvent(int percent) {
+        this.percent = percent;
+    }
+}
