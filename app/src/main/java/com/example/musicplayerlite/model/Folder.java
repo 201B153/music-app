479f352145a54b53848db53e31de866a011c7997
Mukul Kumar <201b162@juetguna.in>
2022-06-01 22:03:41 +0530

    add more files

---
+++ app/src/main/java/com/example/musicplayerlite/model/Folder.java
@@ -0,0 +1,13 @@
+package com.example.musicplayerlite.model;
+
+import com.example.musicplayerlite.helper.ListHelper;
+
+public class Folder {
+    public int songsCount;
+    public String name;
+
+    public Folder(int songsCount, String name) {
+        this.songsCount = songsCount;
+        this.name = ListHelper.ifNull(name);
+    }
+}
